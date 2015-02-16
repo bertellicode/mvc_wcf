259b3cb55bdad4f90c631c3ff7a7af99a0be23d9
bertellicode <diegobertelli.ti@gmail.com>
2015-02-16 02:41:10 -0200

    Commit Inicial

---
+++ ProjetoMVC/Controllers/ProdutoController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProjetoMVC.ProdutoServiceReference;
+
+namespace ProjetoMVC.Controllers
+{
+    public class ProdutoController : Controller
+    {
+
+        readonly ProdutoServiceClient _wcf = new ProdutoServiceClient();
+
+        // GET: Produto
+        public ActionResult Index()
+        {
+            List<Produto> produtos = _wcf.FindAll().ToList();
+            return View(produtos);
+        }
+
+        // GET: Clientes/Details/5
+        public ActionResult Details(int id)
+        {
+            Produto produto = _wcf.Find(id);
+            return View(produto);
+        }
+
+        // GET: Clientes/Create
+        public ActionResult Create()
+        {
+            Produto produto = new Produto();
+            //ViewBag.CategoriaId = new SelectList(_categoriaApp.GetAll(), "CategoriaId", "Nome", produto.CategoriaId);
+            return View(produto);
+        }
+
+        // POST: Clientes/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Produto produto)
+        {
+            if (ModelState.IsValid)
+            {
+                _wcf.New(produto);
+                return RedirectToAction("Index");
+            }
+            //ViewBag.CategoriaId = new SelectList(_categoriaApp.GetAll(), "CategoriaId", "Nome", produto.CategoriaId);
+            return View(produto);
+        }
+
+        // GET: Clientes/Edit/5
+        public ActionResult Edit(int id)
+        {
+            Produto produto = _wcf.Find(id);
+            //ViewBag.CategoriaId = new SelectList(_categoriaApp.GetAll(), "CategoriaId", "Nome", produto.CategoriaId);
+            return View(produto);
+        }
+
+        // POST: Clientes/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Produto produto)
+        {
+            if (ModelState.IsValid)
+            {
+                _wcf.Update(produto);
+                return RedirectToAction("Index");
+            }
+            //ViewBag.CategoriaId = new SelectList(_categoriaApp.GetAll(), "CategoriaId", "Nome", produto.CategoriaId);
+            return View(produto);
+        }
+
+        // GET: Clientes/Delete/5
+        public ActionResult Delete(int id)
+        {
+            Produto produto = _wcf.Find(id);
+            return View(produto);
+        }
+
+        // POST: Clientes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            //Produto produto = _wcf.Find(id);
+            _wcf.DeleteId(id);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
